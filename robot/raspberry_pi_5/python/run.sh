479051179f3b3b7d5c64a5fc41f5443904d810c3
zhangmingjian180 <zhangmingjian180@qq.com>
2025-11-24 13:56:50 +0000

    update robot Raspberry-Pi-5, ffmpeg cmd

--- robot/raspberry_pi_5/python/run.sh
+++ robot/raspberry_pi_5/python/run.sh
@@ -13,5 +13,5 @@ cd $work_dir
 
 source /opt/miniconda3/bin/activate
 python main.py &
-/usr/bin/ffmpeg -f v4l2 -i /dev/video0 -f flv rtmp://${host}:${port}/mytv/${robot_id} > /dev/null 2>&1 &
+/usr/bin/ffmpeg -fflags nobuffer -flags +low_delay -input_format mjpeg -s 1280x720 -framerate 30 -f v4l2 -i /dev/video0 -f flv -fflags nobuffer rtmp://${host}:${port}/mytv/${robot_id} > /dev/null 2>&1 &
 wait
